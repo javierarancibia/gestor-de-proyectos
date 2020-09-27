e87684705760b7ce1059fa9dc517fc74b84acb39
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-27 17:50:14 -0300

    CREATED validations with status default Propuesta

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,15 @@
 class Project < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true, length: { maximum: 160 }
+    validates :start_date, presence: true
+    validates :due_date, presence: true
+    
+
+    enum status: [:Propuesta, :Progreso, :Terminado]
+    
 end
+
+
+
+
+
